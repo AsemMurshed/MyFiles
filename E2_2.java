ae71d019f1c139f82134cfad40b8c66ab1167b7e
AsemMurshed <97526961+AsemMurshed@users.noreply.github.com>
2022-01-28 08:06:27 +0300

    Add files via upload

---
+++ E2_2.java
@@ -0,0 +1,28 @@
+import com.sun.openpisces.Stroker;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by asem on 26/01/2022.
+ */
+public class E2_2 {
+    public static void main(String[] args) {
+        Stacks <Integer>a= new Stacks(5);
+        Scanner in =new Scanner(System.in);
+        System.out.println("input elements ");
+        for (int i = 0; i <5; i++) {
+            a.push(in.nextInt());
+
+        }
+        recursive(a);
+        System.out.println(Arrays.toString(a.x));
+    }
+
+    public static void recursive(Stacks a)   {
+        System.out.println(a.top());
+       if (a.isEmpty()==false){
+        a.pop();
+        recursive(a);}
+    }
+}
---
+++ E2_3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+/**
+ * Created by asem on 26/01/2022.
+ */
+public class E2_3 {
+
+    
+}
